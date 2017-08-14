d0387149bf68b1d3537ceb4b2dc34ddd7d9eb95e
tran-ngocdien <tran-ngocdien@AP0808.local>
2017-08-14 15:40:46 +0900

    - html asset test

---
+++ OverlayViewController/assest/test.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+	//For performance!
+	// document.getElementById("parent").onclick = function (e) {
+	//     //We only want events on the spans, so let's check that!
+	//     if (e.target.tagName.toLowerCase() == "span") {
+	//         this.className = "parent__clicked";
+	//   }
+	// };
+});
+
+var clickedSpan = "";
+
+function isEmpty(str) {
+        return (typeof(str) === "undefined" || str.length == 0);
+}
+
+function setBGColor(e, c) {
+	e.style.backgroundColor = c;
+}
+
+function red(e) {
+    e.style.backgroundColor = "red";
+    if(!isEmpty(clickedSpan)) {
+    	clickedSpan.style.backgroundColor = "white";
+    }
+    clickedSpan = e;
+}
+
+function blue(e) {
+	e.style.backgroundColor = "blue";
+	if(!isEmpty(clickedSpan)) {
+    	clickedSpan.style.backgroundColor = "white";
+    }
+    clickedSpan = e;
+}
+
+
+function yellow(e) {
+	e.style.backgroundColor = "yellow";
+	if(!isEmpty(clickedSpan)) {
+    	clickedSpan.style.backgroundColor = "white";
+    }
+    clickedSpan = e;
+}
+
